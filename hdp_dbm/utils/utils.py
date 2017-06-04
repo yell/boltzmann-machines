c870a9e80528823467c81675986405a785a24090
monsta <yell.bondarenko@gmail.com>
2017-06-04 23:11:52 +0200

    added (stratified) k-fold split

--- hdp_dbm/utils/utils.py
+++ hdp_dbm/utils/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from tqdm import tqdm
 
+from rng import RNG
+
 
 def batch_iter(X, batch_size=10):
     """Divide input data into batches.
@@ -35,6 +37,66 @@ def tbatch_iter(X, batch_size=10):
     for X_b in tqdm(batch_iter(X, batch_size=batch_size),
                     total=n_batches, leave=True, ncols=79):
         yield X_b
+
+
+def make_k_folds(y, n_folds=3, shuffle=True, stratify=True, random_seed=None):
+    """
+    Split data into folds of (approximately) equal size.
+
+    Parameters
+    ----------
+    y : (n_samples,) array-like
+        The target variable for supervised learning problems.
+        Stratification is done based upon the `y` labels.
+    n_folds : int, `n_folds` > 1, optional
+        Number of folds.
+    stratify : bool, optional
+        If True, the folds are made by preserving the percentage of samples
+        for each class. Stratification is done based upon the `y` labels.
+
+    Yields
+    ------
+    fold : np.ndarray
+        Indices for current fold.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> y = np.array([1, 1, 1, 2, 2, 2, 3, 3, 3])
+    >>> for fold in make_k_folds(y, n_folds=3, shuffle=True, stratify=True, random_seed=1337):
+    ...     print y[fold]
+    [3 1 2]
+    [3 2 1]
+    [1 3 2]
+    """
+    n = len(y)
+    rng = RNG(random_seed)
+
+    if not stratify:
+        indices = rng.permutation(n) if shuffle else np.arange(n, dtype=np.int)
+        for fold in np.array_split(indices, n_folds):
+            yield fold
+        return
+
+    # group indices
+    labels_indices = {}
+    for index, label in enumerate(y):
+        if isinstance(label, np.ndarray):
+            label = tuple(label.tolist())
+        if not label in labels_indices:
+            labels_indices[label] = []
+        labels_indices[label].append(index)
+
+    # split all indices label-wisely
+    for label, indices in sorted(labels_indices.items()):
+        labels_indices[label] = np.array_split(indices, n_folds)
+
+    # collect respective splits into folds and shuffle if needed
+    for k in xrange(n_folds):
+        fold = np.concatenate([indices[k] for _, indices in sorted(labels_indices.items())])
+        if shuffle:
+            rng.shuffle(fold)
+        yield fold
 
 
 if __name__ == '__main__':
