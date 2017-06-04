f09410ea3c5a6ca571b214b28b0a18edee779edd
monsta <yell.bondarenko@gmail.com>
2017-06-04 18:01:26 +0200

    added free_energy

--- hdp_dbm/rbm.py
+++ hdp_dbm/rbm.py
@@ -80,12 +80,21 @@ class BaseRBM(TensorFlowModel):
             self._dhb = tf.Variable(tf.zeros((self.n_hidden,)), name='dhb', dtype=tf.float32)
             self._dvb = tf.Variable(tf.zeros((self.n_visible,)), name='dvb', dtype=tf.float32)
 
+    def _propup(self, v):
+        with tf.name_scope('prop_up'):
+            h = tf.matmul(v, self._W) + self._hb
+        return h
+
+    def _propdown(self, h):
+        with tf.name_scope('prop_down'):
+            v = tf.matmul(a=h, b=self._W, transpose_b=True) + self._vb
+        return v
+
     def _sample_h_given_v(self, v):
         """Sample from P(h|v)."""
         with tf.name_scope('sample_h_given_v'):
             with tf.name_scope('h_means'):
-                self._propup = tf.matmul(v, self._W) + self._hb
-                h_means = tf.nn.sigmoid(self._propup)
+                h_means = tf.nn.sigmoid(self._propup(v))
             with tf.name_scope('h_samples'):
                 h_samples = tf.to_float(tf.less(self._h_rand, h_means))
         return h_means, h_samples
@@ -94,8 +103,7 @@ class BaseRBM(TensorFlowModel):
         """Sample from P(v|h)."""
         with tf.name_scope('sample_v_given_h'):
             with tf.name_scope('v_means'):
-                self._propdown = tf.matmul(h, tf.transpose(self._W)) + self._vb
-                v_means = tf.nn.sigmoid(self._propdown)
+                v_means = tf.nn.sigmoid(self._propdown(h))
             with tf.name_scope('v_samples'):
                 v_samples = tf.to_float(tf.less(self._v_rand, v_means))
         return v_means, v_samples
@@ -153,6 +161,17 @@ class BaseRBM(TensorFlowModel):
         # collect summaries
         tf.summary.scalar('msre', msre)
 
+    def _free_energy(self, v):
+        """Compute free energy of a visible vectors `v`."""
+        with tf.name_scope('free_energy'):
+            fe = -tf.einsum('ij,j->i', v, self._vb)
+            fe -= tf.reduce_sum(tf.nn.softplus(self._propup(v)), axis=1)
+            fe = tf.reduce_mean(fe, axis=0)
+        return fe
+
+    def _pseudo_loglik(self, v):
+        """Compute average Pseudo-loglikelihood approximation of `v` as in [3]."""
+        pass
 
     def _make_tf_model(self):
         self._make_init_op()
@@ -173,9 +192,10 @@ class BaseRBM(TensorFlowModel):
         for X_batch in (tbatch_iter if self.verbose else batch_iter)(X, self.batch_size):
             self.iter += 1
             if self.iter % self.compute_metrics_every == 0:
-                _, train_s, train_msre = \
-                    self._tf_session.run([self._train_op, self._tf_merged_summaries, self._msre],
+                _, train_s, train_msre, fe = \
+                    self._tf_session.run([self._train_op, self._tf_merged_summaries, self._msre, self._free_energy(tf.constant(X_batch, dtype='float'))],
                                          feed_dict=self._make_tf_feed_dict(X_batch, is_training=True))
+                print fe
                 self._tf_train_writer.add_summary(train_s, self.iter)
                 train_msres.append(train_msre)
             else:
