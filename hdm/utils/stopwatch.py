6d6c868d20beb2cfa409176238302b80175f405a
monsta <yell.bondarenko@gmail.com>
2017-10-06 23:14:21 +0300

    refactor utils

---
+++ hdm/utils/stopwatch.py
@@ -0,0 +1,63 @@
+import sys
+import time
+
+
+class Stopwatch(object):
+    """
+    A simple cross-platform
+    context-manager stopwatch.
+
+    Examples
+    --------
+    >>> import time
+    >>> with Stopwatch(verbose=True) as s:
+    ...     time.sleep(0.1) # doctest: +ELLIPSIS
+    Elapsed time: 0.10... sec
+    >>> with Stopwatch(verbose=False) as s:
+    ...     time.sleep(0.1)
+    >>> import math
+    >>> math.fabs(s.elapsed() - 0.1) < 0.05
+    True
+    """
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        if sys.platform == 'win32':
+            # on Windows, the best timer is time.clock()
+            self._timer_func = time.clock
+        else:
+            # on most other platforms, the best timer is time.time()
+            self._timer_func = time.time
+        self.reset()
+
+    def __enter__(self, verbose=False):
+        return self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        if self.verbose:
+            print "Elapsed time: {0:.3f} sec".format(self.elapsed())
+
+    def start(self):
+        if not self._is_running:
+            self._start = self._timer_func()
+            self._is_running = True
+        return self
+
+    def stop(self):
+        if self._is_running:
+            self._total += (self._timer_func() - self._start)
+            self._is_running = False
+        return self
+
+    def elapsed(self):
+        if self._is_running:
+            now = self._timer_func()
+            self._total += (now - self._start)
+            self._start = now
+        return self._total
+
+    def reset(self):
+        self._start = 0.
+        self._total = 0.
+        self._is_running = False
+        return self
--- hdm/utils/utils.py
+++ hdm/utils/utils.py
@@ -1,5 +1,3 @@
-import sys
-import time
 import numpy as np
 
 from tqdm import tqdm, tqdm_notebook
@@ -9,70 +7,8 @@ def _is_in_ipython():
 _t = tqdm_notebook if _is_in_ipython() else tqdm
 
 
-class Stopwatch(object):
-    """
-    A simple cross-platform
-    context-manager stopwatch.
-
-    Examples
-    --------
-    >>> import time
-    >>> with Stopwatch(verbose=True) as s:
-    ...     time.sleep(0.1) # doctest: +ELLIPSIS
-    Elapsed time: 0.10... sec
-    >>> with Stopwatch(verbose=False) as s:
-    ...     time.sleep(0.1)
-    >>> import math
-    >>> math.fabs(s.elapsed() - 0.1) < 0.05
-    True
-    """
-    def __init__(self, verbose=False):
-        self.verbose = verbose
-        if sys.platform == 'win32':
-            # on Windows, the best timer is time.clock()
-            self._timer_func = time.clock
-        else:
-            # on most other platforms, the best timer is time.time()
-            self._timer_func = time.time
-        self.reset()
-
-    def __enter__(self, verbose=False):
-        return self.start()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
-        if self.verbose:
-            print "Elapsed time: {0:.3f} sec".format(self.elapsed())
-
-    def start(self):
-        if not self._is_running:
-            self._start = self._timer_func()
-            self._is_running = True
-        return self
-
-    def stop(self):
-        if self._is_running:
-            self._total += (self._timer_func() - self._start)
-            self._is_running = False
-        return self
-
-    def elapsed(self):
-        if self._is_running:
-            now = self._timer_func()
-            self._total += (now - self._start)
-            self._start = now
-        return self._total
-
-    def reset(self):
-        self._start = 0.
-        self._total = 0.
-        self._is_running = False
-        return self
-
-
 def write_during_training(s):
     tqdm.write(s)
-
 
 def batch_iter(X, batch_size=10, verbose=False):
     """Divide input data into batches, with optional
@@ -103,7 +39,6 @@ def batch_iter(X, batch_size=10, verbose=False):
     if verbose: gen = _t(gen, leave=False, ncols=64, desc='epoch')
     for i in gen:
         yield X[i*batch_size:(i + 1)*batch_size]
-
 
 def epoch_iter(start_epoch, max_epoch, verbose=False):
     gen = xrange(start_epoch + 1, max_epoch + 1)
