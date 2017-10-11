a16770360f35bf257fc2a87e2810510d97b555d6
monsta <yell.bondarenko@gmail.com>
2017-10-11 22:13:37 +0200

    update DBM MNIST experiment

--- examples/dbm_mnist.py
+++ examples/dbm_mnist.py
@@ -28,20 +28,24 @@ from hdm.utils.dataset import load_mnist
 def main():
     # training settings
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--n-train', type=int, default=55000, metavar='N',
+
+    # data-related
+    parser.add_argument('--n-train', type=int, default=57600, metavar='N',
                         help='number of training examples')
-    parser.add_argument('--n-val', type=int, default=5000, metavar='N',
+    parser.add_argument('--n-val', type=int, default=2400, metavar='N',
                         help='number of validation examples')
 
+    # common
     parser.add_argument('--n-hiddens', type=int, default=[512, 1024], metavar='N', nargs='+',
                         help='numbers of hidden units')
     parser.add_argument('--epochs', type=int, default=[64, 140, 300], metavar='N', nargs='+',
                         help='number of epochs to train')
-    parser.add_argument('--batch-size', type=int, default=48, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=[48], metavar='N', nargs='+',
                         help='input batch size for training')
     parser.add_argument('--increase-n-gibbs-steps-every', type=int, default=20, metavar='I',
                         help='increase number of Gibbs steps every specified number of epochs for RBM #2')
 
+    # dirpaths
     parser.add_argument('--rbm1-dirpath', type=str, default='../models/dbm_mnist_rbm1/', metavar='DIRPATH',
                         help='directory path to save RBM #1')
     parser.add_argument('--rbm2-dirpath', type=str, default='../models/dbm_mnist_rbm2/', metavar='DIRPATH',
@@ -53,7 +57,28 @@ def main():
                         help='directory path to load trained RBM #1')
     parser.add_argument('--load-rbm2', type=str, default=None, metavar='DIRPATH',
                         help='directory path to load trained RBM #2')
+    parser.add_argument('--load-dbm', type=str, default=None, metavar='DIRPATH',
+                        help='directory path to load trained DBM')
+
+    # DBM-related
+    parser.add_argument('--n-particles', type=int, default=100, metavar='M',
+                        help='number of persistent Markov chains')
+    parser.add_argument('--n-gibbs-steps', type=int, default=1, metavar='N',
+                        help='number of Gibbs steps for PCD')
+    parser.add_argument('--max-mf-updates', type=int, default=50, metavar='N',
+                        help='maximum number of mean-field updates per weight update')
+    parser.add_argument('--mf-tol', type=float, default=1e-8, metavar='TOL',
+                        help='mean-field tolerance')
+    parser.add_argument('--lr', type=float, default=5e-4, metavar='LR',
+                        help='initial learning rate')
+    parser.add_argument('--l2', type=float, default=1e-8, metavar='L2',
+                        help='L2 weight decay coefficient')
+    parser.add_argument('--max-norm', type=float, default=8., metavar='C',
+                        help='maximum norm constraint')
+
     args = parser.parse_args()
+    if len(args.epochs) == 1: args.epochs *= 3
+    if len(args.batch_size) == 1: args.batch_size *= 3
 
     # prepare data
     X, _ = load_mnist(mode='train', path='../data/')
@@ -78,8 +103,8 @@ def main():
                             learning_rate=0.05,
                             momentum=[0.5] * 5 + [0.9],
                             max_epoch=args.epochs[0],
-                            batch_size=args.batch_size,
-                            L2=1e-3,
+                            batch_size=args.batch_size[0],
+                            l2=1e-3,
                             sample_h_states=True,
                             sample_v_states=True,
                             sparsity_cost=0.,
@@ -118,12 +143,12 @@ def main():
             learning_rate=rbm2_learning_rate,
             momentum=[0.5] * 5 + [0.9],
             max_epoch=args.increase_n_gibbs_steps_every,
-            batch_size=args.batch_size,
-            L2=1e-4,
+            batch_size=args.batch_size[1],
+            l2=1e-4,
             sample_h_states=True,
             sample_v_states=True,
             sparsity_cost=0.,
-            dbm_last=True,  # !!!
+            dbm_last=True, # !!!
             metrics_config=dict(
                 msre=True,
                 pll=True,
@@ -160,7 +185,39 @@ def main():
     Q = rbm2.transform(Z)
     print Q.shape
 
-    # DBM <- (rbm1, rbm2)
+    # jointly train DBM
+    if args.load_dbm:
+        print "\nLoading DBM ...\n\n"
+        dbm = DBM.load_model(args.load_dbm)
+        dbm.load_rbms([rbm1, rbm2]) # !!!
+    else:
+        print "\nTraining DBM ...\n\n"
+        dbm = DBM(rbms=[rbm1, rbm2],
+                  n_particles=args.n_particles,
+                  v_particle_init=X[:args.n_particles].copy(),
+                  h_particles_init=(Z[:args.n_particles].copy(),
+                                    Q[:args.n_particles].copy()),
+                  n_gibbs_steps=args.n_gibbs_steps,
+                  max_mf_updates=args.max_mf_updates,
+                  mf_tol=args.mf_tol,
+                  learning_rate=np.geomspace(args.lr, 1e-5, args.epochs[2]),
+                  momentum=np.geomspace(0.5, 0.9, 8),
+                  max_epoch=args.epochs[2],
+                  batch_size=args.batch_size[2],
+                  l2=args.l2,
+                  max_norm=args.max_norm,
+                  sample_v_states=True,
+                  sample_h_states=(True, True),
+                  train_metrics_every_iter=100,
+                  val_metrics_every_epoch=1,
+                  random_seed=1337,
+                  verbose=True,
+                  tf_dtype='float32',
+                  save_after_each_epoch=True,
+                  tf_saver_params=dict(max_to_keep=1),
+                  model_path=args.dbm_dirpath)
+        dbm.fit(X_train, X_val)
+
 
 
 if __name__ == '__main__':
