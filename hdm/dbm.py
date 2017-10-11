c9be65ccc05045bb9fecd8979753782f5eb934c8
monsta <yell.bondarenko@gmail.com>
2017-10-11 22:20:08 +0200

    add numpy arrays serialization

--- hdm/dbm.py
+++ hdm/dbm.py
@@ -585,3 +585,10 @@ class DBM(TensorFlowModel):
         if save_model:
             self._save_model()
         return v
+
+    def _serialize(self, params):
+        for k, v in params.items():
+            if isinstance(v, np.ndarray):
+                # noinspection PyUnresolvedReferences
+                params[k] = v.tolist()
+        return params
