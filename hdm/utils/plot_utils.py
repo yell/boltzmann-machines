c503bb1b97d09e5691b2d5a198319f197f4fa90e
monsta <yell.bondarenko@gmail.com>
2017-10-16 14:40:51 +0200

    refactor `plot_matrices`

--- hdm/utils/plot_utils.py
+++ hdm/utils/plot_utils.py
@@ -3,24 +3,50 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 
-def plot_matrices(W, n_width=10, n_height=10, shape=None, title=None, imshow_params=None):
-    """Draw grid of matrices represented by rows of `W`."""
+def plot_matrices(X, n_width=10, n_height=10, shape=None, title=None, title_params=None, imshow_params=None):
+    """Draw grid of matrices represented by rows of `W`.
+
+    Returns
+    -------
+    im : matplotlib.image.AxesImage
+    Z : (`n_height` * `shape`[0], `n_width` * `shape`[1], `shape`[2]) np.ndarray
+        reshaped `X` for plotting
+    """
+
+    # check params
+    X = np.asarray(X)
+
+    if shape is None:
+        shape = X.shape[1:]
+    title_params = title_params or {}
+    title_params.setdefault('fontsize', 22)
     imshow_params = imshow_params or {}
     imshow_params.setdefault('interpolation', 'nearest')
-    for i in xrange(n_height * n_width):
-        matrix = W[i]
-        if shape is not None:
-            matrix = matrix.reshape(shape)
-        ax = plt.subplot(n_height, n_width, i + 1)
-        for d in ('bottom', 'top', 'left', 'right'):
-            ax.spines[d].set_linewidth(2.)
-        plt.tick_params(axis='both', which='both',
+
+    # reshape `X`
+    Y = X[:(n_width * n_height), ...].copy()
+    if len(shape) == 2:
+        shape = (shape[0], shape[1], 1)
+    Y = Y.reshape(-1, *shape)
+    Z = np.zeros((n_height * shape[0], n_width * shape[1], shape[2]))
+    for i in xrange(n_height):
+        for j in xrange(n_width):
+            ind_Y = n_height * i + j
+            if ind_Y < len(Y):
+                Z[i * shape[0]:(i + 1) * shape[0],
+                  j * shape[1]:(j + 1) * shape[1], ...] = Y[ind_Y, ...]
+    if Z.shape[2] == 1:
+        Z = Z[:, :, 0]
+
+    # plot reshaped `X`
+    im = plt.imshow(Z, **imshow_params)
+    if title:
+        im.axes.set_title(title, **title_params)
+    im.axes.tick_params(axis='both', which='both',
                         bottom='off', top='off', left='off', right='off',
                         labelbottom='off', labelleft='off', labelright='off')
-        plt.imshow(matrix, **imshow_params)
-    if title:
-        plt.suptitle(title, fontsize=22)
-    plt.subplots_adjust(wspace=0, hspace=0)
+    return im, Z
+
 
 def plot_confusion_matrix(C, labels=None, labels_fontsize=None, **heatmap_params):
     # default params
