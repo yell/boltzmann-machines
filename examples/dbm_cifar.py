2f698875cbad1506c045642fee9ba834af27a6e0
monsta <yell.bondarenko@gmail.com>
2017-10-18 16:45:55 +0200

    refactor small RBMs training

--- examples/dbm_cifar.py
+++ examples/dbm_cifar.py
@@ -18,9 +18,6 @@ print __doc__
 import os
 import argparse
 import numpy as np
-if not 'DISPLAY' in os.environ:
-    import matplotlib
-    matplotlib.use('Agg')
 
 import env
 from hdm.dbm import DBM
@@ -29,6 +26,143 @@ from hdm.utils import RNG, Stopwatch
 from hdm.utils.augmentation import shift, horizontal_mirror
 from hdm.utils.dataset import (load_cifar10,
                                im_flatten, im_unflatten)
+
+
+def make_small_rbms(X_train, X_val, small_rbm_config, args):
+    small_rbms = []
+
+    # first 16 ...
+    for i in xrange(4):
+        for j in xrange(4):
+            rbm_id = 4 * i + j
+            rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
+
+            if os.path.isdir(rbm_dirpath):
+                print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
+                rbm = GaussianRBM.load_model(rbm_dirpath)
+            else:
+                print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
+                X_patches = X_train[:, 8 * i:8 * (i + 1),
+                            8 * j:8 * (j + 1), :]
+                X_patches_val = X_val[:, 8 * i:8 * (i + 1),
+                                8 * j:8 * (j + 1), :]
+                X_patches = im_flatten(X_patches)
+                X_patches_val = im_flatten(X_patches_val)
+
+                rbm = GaussianRBM(random_seed=9000 + rbm_id,
+                                  model_path=rbm_dirpath,
+                                  **small_rbm_config)
+                rbm.fit(X_patches, X_patches_val)
+            small_rbms.append(rbm)
+
+    # next 9 ...
+    for i in xrange(3):
+        for j in xrange(3):
+            rbm_id = 16 + 3 * i + j
+            rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
+
+            if os.path.isdir(rbm_dirpath):
+                print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
+                rbm = GaussianRBM.load_model(rbm_dirpath)
+            else:
+                print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
+                X_patches = X_train[:, 4 + 8 * i:4 + 8 * (i + 1),
+                            4 + 8 * j:4 + 8 * (j + 1), :]
+                X_patches_val = X_val[:, 4 + 8 * i:4 + 8 * (i + 1),
+                                4 + 8 * j:4 + 8 * (j + 1), :]
+                X_patches = im_flatten(X_patches)
+                X_patches_val = im_flatten(X_patches_val)
+
+                rbm = GaussianRBM(random_seed=9000 + rbm_id,
+                                  model_path=rbm_dirpath,
+                                  **small_rbm_config)
+                rbm.fit(X_patches, X_patches_val)
+            small_rbms.append(rbm)
+
+    # ... the last one
+    rbm_id = 25
+    rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
+
+    if os.path.isdir(rbm_dirpath):
+        print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
+        rbm = GaussianRBM.load_model(rbm_dirpath)
+    else:
+        print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
+        X_patches = X_train.copy()  # (N, 32, 32, 3)
+        X_patches = X_patches.transpose(0, 3, 1, 2)  # (N, 3, 32, 32)
+        X_patches = X_patches.reshape((-1, 3, 4, 8, 4, 8)).mean(axis=4).mean(axis=2)  # (N, 3, 8, 8)
+        X_patches = X_patches.transpose(0, 2, 3, 1)  # (N, 8, 8, 3)
+        X_patches = im_flatten(X_patches)  # (N, 8*8*3)
+
+        X_patches_val = X_val.copy()
+        X_patches_val = X_patches_val.transpose(0, 3, 1, 2)
+        X_patches_val = X_patches_val.reshape((-1, 3, 4, 8, 4, 8)).mean(axis=4).mean(axis=2)
+        X_patches_val = X_patches_val.transpose(0, 2, 3, 1)
+        X_patches_val = im_flatten(X_patches_val)
+
+        rbm = GaussianRBM(random_seed=9000 + rbm_id,
+                          model_path=rbm_dirpath,
+                          **small_rbm_config)
+        rbm.fit(X_patches, X_patches_val)
+    small_rbms.append(rbm)
+    return small_rbms
+
+
+def make_large_weights(small_rbms):
+    W = np.zeros((300 * 26, 32, 32, 3), dtype=np.float32)
+    vb = np.zeros((32, 32, 3))
+    hb = np.zeros(300 * 26)
+
+    for i in xrange(4):
+        for j in xrange(4):
+            rbm_id = 4 * i + j
+            weights = small_rbms[rbm_id].get_tf_params(scope='weights')
+            W_small = weights['W']
+            W_small = W_small.T  # (300, 192)
+            W_small = im_unflatten(W_small)  # (300, 8, 8, 3)
+            W[300 * rbm_id: 300 * (rbm_id + 1), 8 * i:8 * (i + 1),
+                                                8 * j:8 * (j + 1), :] = W_small
+            vb[8 * i:8 * (i + 1),
+               8 * j:8 * (j + 1), :] += im_unflatten(weights['vb'])
+            hb[300 * rbm_id: 300 * (rbm_id + 1)] = weights['hb']
+
+    for i in xrange(3):
+        for j in xrange(3):
+            rbm_id = 16 + 3 * i + j
+            weights = small_rbms[rbm_id].get_tf_params(scope='weights')
+            W_small = weights['W']
+            W_small = W_small.T
+            W_small = im_unflatten(W_small)
+            W[300 * rbm_id: 300 * (rbm_id + 1), 4 + 8 * i:4 + 8 * (i + 1),
+                                                4 + 8 * j:4 + 8 * (j + 1), :] = W_small
+            vb[4 + 8 * i:4 + 8 * (i + 1),
+               4 + 8 * j:4 + 8 * (j + 1), :] += im_unflatten(weights['vb'])
+            hb[300 * rbm_id: 300 * (rbm_id + 1)] = weights['hb']
+
+    weights = small_rbms[25].get_tf_params(scope='weights')
+    W_small = weights['W']
+    W_small = W_small.T
+    W_small = im_unflatten(W_small)
+    vb_small = im_unflatten(weights['vb'])
+    for i in xrange(8):
+        for j in xrange(8):
+            U = W_small[:, i, j, :]
+            U = np.expand_dims(U, -1)
+            U = np.expand_dims(U, -1)
+            U = U.transpose(0, 2, 3, 1)
+            W[-300:, 4 * i:4 * (i + 1),
+                     4 * j:4 * (j + 1), :] = U / 16.
+            vb[4 * i:4 * (i + 1),
+               4 * j:4 * (j + 1), :] += vb_small[i, j, :].reshape((1, 1, 3)) / 16.
+            hb[-300:] = weights['hb']
+
+    W = im_flatten(W)
+    W = W.T
+    vb /= 2.
+    vb[4:-4, 4:-4, :] /= 1.5
+    vb = im_flatten(vb)
+
+    return W, vb, hb
 
 
 def main():
@@ -62,6 +196,13 @@ def main():
                         help='controls the amount of sparsity penalty')
     parser.add_argument('--small-dirpath-prefix', type=str, default='../models/rbm_cifar_small_', metavar='PREFIX',
                         help='directory path prefix to save RBMs trained on patches')
+
+    # common for RBMs and DBM
+    parser.add_argument('--epochs', type=int, default=[64, 120, 500], metavar='N', nargs='+',
+                        help='number of epochs to train')
+    parser.add_argument('--batch-size', type=int, default=[48, 48, 100], metavar='B', nargs='+',
+                        help='input batch size for training, `--n-train` and `--n-val`' + \
+                             'must be divisible by this number (for DBM)')
 
     args = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
@@ -146,8 +287,7 @@ def main():
     X_train = im_unflatten(X_train)
     X_val = im_unflatten(X_val)
 
-    small_rbms = []
-    rbm_small_config = dict(n_visible=8*8*3,
+    small_rbm_config = dict(n_visible=8*8*3,
                             n_hidden=300,
                             sigma=1.,
                             W_init=0.001,
@@ -179,137 +319,57 @@ def main():
                             tf_dtype='float32',
                             tf_saver_params=dict(max_to_keep=1))
 
-    # first 16 ...
-    for i in xrange(4):
-        for j in xrange(4):
-            rbm_id = 4 * i + j
-            rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
-
-            if os.path.isdir(rbm_dirpath):
-                print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
-                rbm = GaussianRBM.load_model(rbm_dirpath)
-            else:
-                print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
-                X_patches   = X_train[:, 8 * i:8 * (i + 1),
-                                         8 * j:8 * (j + 1), :]
-                X_patches_val = X_val[:, 8 * i:8 * (i + 1),
-                                         8 * j:8 * (j + 1), :]
-                X_patches     = im_flatten(X_patches)
-                X_patches_val = im_flatten(X_patches_val)
-
-                rbm = GaussianRBM(random_seed=9000 + rbm_id,
-                                  model_path=rbm_dirpath,
-                                  **rbm_small_config)
-                rbm.fit(X_patches, X_patches_val)
-            small_rbms.append(rbm)
-
-    # next 9 ...
-    for i in xrange(3):
-        for j in xrange(3):
-            rbm_id = 16 + 3 * i + j
-            rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
-
-            if os.path.isdir(rbm_dirpath):
-                print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
-                rbm = GaussianRBM.load_model(rbm_dirpath)
-            else:
-                print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
-                X_patches   = X_train[:, 4 + 8 * i:4 + 8 * (i + 1),
-                                         4 + 8 * j:4 + 8 * (j + 1), :]
-                X_patches_val = X_val[:, 4 + 8 * i:4 + 8 * (i + 1),
-                                         4 + 8 * j:4 + 8 * (j + 1), :]
-                X_patches = im_flatten(X_patches)
-                X_patches_val = im_flatten(X_patches_val)
-
-                rbm = GaussianRBM(random_seed=9000 + rbm_id,
-                                  model_path=rbm_dirpath,
-                                  **rbm_small_config)
-                rbm.fit(X_patches, X_patches_val)
-            small_rbms.append(rbm)
-
-    # ... the last one
-    rbm_id = 25
-    rbm_dirpath = args.small_dirpath_prefix + str(rbm_id) + '/'
-
-    if os.path.isdir(rbm_dirpath):
-        print "\nLoading small RBM #{0} ...\n\n".format(rbm_id)
-        rbm = GaussianRBM.load_model(rbm_dirpath)
-    else:
-        print "\nTraining small RBM #{0} ...\n\n".format(rbm_id)
-        X_patches = X_train.copy() # (N, 32, 32, 3)
-        X_patches = X_patches.transpose(0, 3, 1, 2) # (N, 3, 32, 32)
-        X_patches = X_patches.reshape((-1, 3, 4, 8, 4, 8)).mean(axis=4).mean(axis=2)  # (N, 3, 8, 8)
-        X_patches = X_patches.transpose(0, 2, 3, 1)  # (N, 8, 8, 3)
-        X_patches = im_flatten(X_patches) # (N, 8*8*3)
-
-        X_patches_val = X_val.copy()
-        X_patches_val = X_patches_val.transpose(0, 3, 1, 2)
-        X_patches_val = X_patches_val.reshape((-1, 3, 4, 8, 4, 8)).mean(axis=4).mean(axis=2)
-        X_patches_val = X_patches_val.transpose(0, 2, 3, 1)
-        X_patches_val = im_flatten(X_patches_val)
-
-        rbm = GaussianRBM(random_seed=9000 + rbm_id,
-                          model_path=rbm_dirpath,
-                          **rbm_small_config)
-        rbm.fit(X_patches, X_patches_val)
-    small_rbms.append(rbm)
+    small_rbms = make_small_rbms(X_train, X_val, small_rbm_config, args)
 
     # assemble large weight matrix and biases
-    print "\nAssembling weights for large Gaussian RBM ...\n\n".format(rbm_id)
-    W = np.zeros((300 * 26, 32, 32, 3), dtype=np.float32)
-    vb = np.zeros((32, 32, 3))
-    hb = np.zeros(300 * 26)
-    for i in xrange(4):
-        for j in xrange(4):
-            rbm_id = 4 * i + j
-            weights = small_rbms[rbm_id].get_tf_params(scope='weights')
-            W_small = weights['W']
-            W_small = W_small.T # (300, 192)
-            W_small = im_unflatten(W_small) # (300, 8, 8, 3)
-            W[300 * rbm_id : 300 * (rbm_id + 1), 8 * i:8 * (i + 1),
-                                                 8 * j:8 * (j + 1), :] = W_small
-            vb[8 * i:8 * (i + 1),
-               8 * j:8 * (j + 1), :] += im_unflatten(weights['vb'])
-            hb[300 * rbm_id : 300 * (rbm_id + 1)] = weights['hb']
-    for i in xrange(3):
-        for j in xrange(3):
-            rbm_id = 16 + 3 * i + j
-            weights = small_rbms[rbm_id].get_tf_params(scope='weights')
-            W_small = weights['W']
-            W_small = W_small.T
-            W_small = im_unflatten(W_small)
-            W[300 * rbm_id : 300 * (rbm_id + 1), 4 + 8 * i:4 + 8 * (i + 1),
-                                                 4 + 8 * j:4 + 8 * (j + 1), :] = W_small
-            vb[4 + 8 * i:4 + 8 * (i + 1),
-               4 + 8 * j:4 + 8 * (j + 1), :] += im_unflatten(weights['vb'])
-            hb[300 * rbm_id: 300 * (rbm_id + 1)] = weights['hb']
-    weights = small_rbms[-1].get_tf_params(scope='weights')
-    W_small = weights['W']
-    W_small = W_small.T
-    W_small = im_unflatten(W_small)
-    vb_small = im_unflatten(weights['vb'])
-    for i in xrange(8):
-        for j in xrange(8):
-            U = W_small[:, i, j, :]
-            U = np.expand_dims(U, -1)
-            U = np.expand_dims(U, -1)
-            U = U.transpose(0, 2, 3, 1)
-            W[-300:, 4 * i:4 * (i + 1),
-                     4 * j:4 * (j + 1), :] = U/16.
-            vb[4 * i:4 * (i + 1),
-               4 * j:4 * (j + 1), :] += vb_small[i, j, :].reshape((1, 1, 3))/16.
-            hb[-300:] = weights['hb']
-    W = im_flatten(W)
-    W = W.T
-    vb /= 2.
-    vb[4:-4, 4:-4, :] /= 1.5
-    vb = im_flatten(vb)
+    print "\nAssembling weights for large Gaussian RBM ...\n\n"
+    W, vb, hb = make_large_weights( small_rbms )
     print W.shape
     print vb.shape
     print hb.shape
 
     # pre-train large Gaussian RBM
+    # grbm = GaussianRBM(n_visible=32*32*3,
+    #                    n_hidden=300*26,
+    #                    sigma=1.,
+    #                    W_init=W,
+    #                    vb_init=vb,
+    #                    hb_init=hb,
+    #                    n_gibbs_steps=1,
+    #
+    #                         learning_rate=args.small_lr,
+    #                         momentum=np.geomspace(0.5, 0.9, 8),
+    #                         max_epoch=args.small_epochs,
+    #                         batch_size=args.small_batch_size,
+    #                         l2=args.small_l2,
+    #                         sample_v_states=True,
+    #                         sample_h_states=True,
+    #                         sparsity_target=args.small_sparsity_target,
+    #                         sparsity_cost=args.small_sparsity_cost,
+    #                         dbm_first=True,  # !!!
+    #                         metrics_config=dict(
+    #                             msre=True,
+    #                             feg=True,
+    #                             train_metrics_every_iter=1000,
+    #                             val_metrics_every_epoch=2,
+    #                             feg_every_epoch=2,
+    #                             n_batches_for_feg=50,
+    #                         ),
+    #                         verbose=True,
+    #                         display_filters=12,
+    #                         v_shape=(8, 8, 3),
+    #                         display_hidden_activations=24,
+    #                         tf_dtype='float32',
+    #                         tf_saver_params=dict(max_to_keep=1))
 
+    ################ NETFLIX PAPER #################
+    """
+    Lr 0.01
+    momentum 0.9
+    L2 1e-3
+    W_std 0.01
+    CD increased over time (for RBM #2)
+    """
 
 
 if __name__ == '__main__':
