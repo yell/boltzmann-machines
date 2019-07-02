7251841f55b9713cae22a5649a95ae901cc94403
kmckiern <kmckiern@stanford.edu>
2019-07-01 19:47:52 -0700

    rm all extra stuff

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:2
-
-RUN mkdir -p /var/app
-WORKDIR /var/app
-COPY . /var/app
-
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
--- data/fetch_cifar10.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-wget "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-tar -xzvf cifar-10-python.tar.gz && rm cifar-10-python.tar.gz
